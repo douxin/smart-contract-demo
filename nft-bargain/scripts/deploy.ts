2638c7a212027fd96b15c2bfca5e7d6dc1f33932
Xin Dou <master2dou@gmail.com>
2022-09-19 12:17:56 +0800

    update deploy script

--- nft-bargain/scripts/deploy.ts
+++ nft-bargain/scripts/deploy.ts
@@ -1,23 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const MIN_BARGAIN_NUM = 2;
+    const Bargain = await ethers.getContractFactory("NFTBargain");
+    const bargain = await Bargain.deploy(MIN_BARGAIN_NUM);
+    await bargain.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    console.log(`deployed NFTBargain contract with min bargain number set to ${MIN_BARGAIN_NUM}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
