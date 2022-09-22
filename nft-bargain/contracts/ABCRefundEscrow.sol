d73233a1714a16cdcc730b078d16c9ec8804c5ac
Xin Dou <master2dou@gmail.com>
2022-09-22 13:27:01 +0800

    refact refund escrow

--- nft-bargain/contracts/ABCRefundEscrow.sol
+++ nft-bargain/contracts/ABCRefundEscrow.sol
@@ -2,62 +2,49 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ABCToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./TRefundEscrow.sol";
 
-contract ABCRefundEscrow is Ownable {
+contract ABCRefundEscrow is Ownable, TRefundEscrow {
     // address of deployed ABCToken contract
-    address constant ABC_TOKEN_ADDRESS = 0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692;
+    address constant ABC_TOKEN_ADDRESS =
+        0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692;
 
-    mapping(address => uint256) private _deposits;
+    IERC20 private _token;
 
-    enum State {
-        Active,
-        Refunding,
-        Closed
+    constructor(address payable beneficiary_) TRefundEscrow(beneficiary_) {
+        _token = IERC20(ABC_TOKEN_ADDRESS);
     }
 
-    State private _state;
-    address payable private immutable _beneficiary;
-
-    constructor(address payable beneficiary_) {
-        require(beneficiary_ != address(0), "RefundEscrow: beneficiary is the zero address");
-        _beneficiary = beneficiary_;
-        _state = State.Active;
+    function deposit(address payee, uint256 amount)
+        public
+        payable
+        override
+        onlyOwner
+    {
+        super.deposit(payee, amount);
     }
 
-    function depositsOf(address payee) public view returns (uint256) {
-        return _deposits[payee];
+    function withdraw(address payable payee) public override {
+        require(
+            state() == State.Refunding,
+            "can only withdraw while refunding"
+        );
+        uint256 payment = depositsOf(payee);
+
+        emptyDeposit(payee);
+
+        _token.transferFrom(address(this), payee, payment);
     }
 
-    function deposit(address payee, uint256 amount) public onlyOwner {
-        require(_state == State.Active, "RefundEscrow: can only deposit while active");
-        _deposits[payee] += amount;
-    }
-
-    function withdraw(address payee) public onlyOwner {
-        require(_state == State.Refunding, "can only withdraw while refunding");
-        uint256 payment = _deposits[payee];
-
-        _deposits[payee] = 0;
-
-        ABCToken(ABC_TOKEN_ADDRESS).transferFrom(address(this), payee, payment);
-    }
-
-    function close() public virtual onlyOwner {
-        require(_state == State.Active || _state == State.Refunding, "RefundEscrow: can only close while active or refunding");
-        _state = State.Closed;
-    }
-
-    function enableRefunds() public virtual onlyOwner {
-        require(_state == State.Active, "RefundEscrow: can only enable refunds while active");
-        _state = State.Refunding;
-    }
-
-    function beneficiaryWithdraw() public virtual {
-        require(_state == State.Closed, "RefundEscrow: beneficiary can only withdraw while closed");
-        uint256 balanceOfRest = ABCToken(ABC_TOKEN_ADDRESS).balanceOf(address(this));
+    function beneficiaryWithdraw() public override {
+        require(
+            state() == State.Closed,
+            "RefundEscrow: beneficiary can only withdraw while closed"
+        );
+        uint256 balanceOfRest = _token.balanceOf(address(this));
         require(balanceOfRest > 0, "rest balance should greater than 0");
-        
-        ABCToken(ABC_TOKEN_ADDRESS).transferFrom(address(this), _beneficiary, balanceOfRest);
+
+        _token.transferFrom(address(this), beneficiary(), balanceOfRest);
     }
 }
---
+++ nft-bargain/contracts/TRefundEscrow.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract TRefundEscrow is Ownable {
+    event Deposited(address indexed payee, uint256 weiAmount);
+    event Withdrawn(address indexed payee, uint256 weiAmount);
+
+    mapping(address => uint256) private _deposits;
+
+    enum State {
+        Active,
+        Refunding,
+        Closed
+    }
+
+    State private _state;
+    address payable private immutable _beneficiary;
+
+    constructor(address payable beneficiary_) {
+        require(beneficiary_ != address(0), "RefundEscrow: beneficiary is the zero address");
+        _beneficiary = beneficiary_;
+        _state = State.Active;
+    }
+
+    function state() public view returns (State) {
+        return _state;
+    }
+
+    function beneficiary() public view returns (address payable) {
+        return _beneficiary;
+    }
+
+    function depositsOf(address payee) public view returns (uint256) {
+        return _deposits[payee];
+    }
+
+    function close() public virtual onlyOwner {
+        require(_state == State.Active || _state == State.Refunding, "RefundEscrow: can only close while active or refunding");
+        _state = State.Closed;
+    }
+
+    function enableRefunds() public virtual onlyOwner {
+        require(_state == State.Active, "RefundEscrow: can only enable refunds while active");
+        _state = State.Refunding;
+    }
+
+    function emptyDeposit(address payee) public onlyOwner {
+        _deposits[payee] = 0;
+    }
+
+    function deposit(address payee, uint256 amount) public payable virtual onlyOwner {
+        require(state() == State.Active, "RefundEscrow: can only deposit while active");
+        _deposits[payee] += amount;
+    }
+
+    function withdraw(address payable payee) public virtual;
+    function beneficiaryWithdraw() public virtual;
+}
