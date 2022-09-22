36456f6abd6b3e0b6910982002f22898837c8780
Xin Dou <master2dou@gmail.com>
2022-09-22 11:20:17 +0800

    add ABC support

--- nft-bargain/contracts/Reward.sol
+++ nft-bargain/contracts/Reward.sol
@@ -2,106 +2,23 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/escrow/ConditionalEscrow.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./NFTBargain.sol";
-
-/**
- * 修改 openzeppelin close 方法
- * 原方法只能在 _state 为 Active 情况下才能关闭，导致开启了 Refund 之后无法 Close
- * 而且因为 _state 是 private，无法继承来修改
- */
-contract RefundEscrow is ConditionalEscrow {
-    using Address for address payable;
-
-    enum State {
-        Active,
-        Refunding,
-        Closed
-    }
-
-    event RefundsClosed();
-    event RefundsEnabled();
-
-    State private _state;
-    address payable private immutable _beneficiary;
-
-    /**
-     * @dev Constructor.
-     * @param beneficiary_ The beneficiary of the deposits.
-     */
-    constructor(address payable beneficiary_) {
-        require(beneficiary_ != address(0), "RefundEscrow: beneficiary is the zero address");
-        _beneficiary = beneficiary_;
-        _state = State.Active;
-    }
-
-    /**
-     * @return The current state of the escrow.
-     */
-    function state() public view virtual returns (State) {
-        return _state;
-    }
-
-    /**
-     * @return The beneficiary of the escrow.
-     */
-    function beneficiary() public view virtual returns (address payable) {
-        return _beneficiary;
-    }
-
-    /**
-     * @dev Stores funds that may later be refunded.
-     * @param refundee The address funds will be sent to if a refund occurs.
-     */
-    function deposit(address refundee) public payable virtual override {
-        require(state() == State.Active, "RefundEscrow: can only deposit while active");
-        super.deposit(refundee);
-    }
-
-    /**
-     * @dev Allows for the beneficiary to withdraw their funds, rejecting
-     * further deposits.
-     * 将 `_state == State.Active` 修改为 `_state == State.Active || _state == State.Refunding`
-     */
-    function close() public virtual onlyOwner {
-        require(_state == State.Active || _state == State.Refunding, "RefundEscrow: can only close while active or refunding");
-        _state = State.Closed;
-        emit RefundsClosed();
-    }
-
-    /**
-     * @dev Allows for refunds to take place, rejecting further deposits.
-     */
-    function enableRefunds() public virtual onlyOwner {
-        require(state() == State.Active, "RefundEscrow: can only enable refunds while active");
-        _state = State.Refunding;
-        emit RefundsEnabled();
-    }
-
-    /**
-     * @dev Withdraws the beneficiary's funds.
-     */
-    function beneficiaryWithdraw() public virtual {
-        require(state() == State.Closed, "RefundEscrow: beneficiary can only withdraw while closed");
-        beneficiary().sendValue(address(this).balance);
-    }
-
-    /**
-     * @dev Returns whether refundees can withdraw their deposits (be refunded). The overridden function receives a
-     * 'payee' argument, but we ignore it here since the condition is global, not per-payee.
-     */
-    function withdrawalAllowed(address) public view override returns (bool) {
-        return state() == State.Refunding;
-    }
-}
+import "./RefundEscrow.sol";
+import "./ABCRefundEscrow.sol";
+import "./ABCToken.sol";
 
 contract Reward is Ownable {
-    // address of deployed NFT contract
+    // address of deployed NFTBargain contract
     address constant NFT_BARGAIN_ADDRESS = 0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692;
+
+    // address of deployed ABCToken contract
+    address constant ABC_TOKEN_ADDRESS = 0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692;
+
     using SafeMath for uint256;
 
     RefundEscrow private immutable _refundEscrow;
+    ABCRefundEscrow private immutable _abcRefundEscrow;
 
     // NFT mint 数量
     uint256 private _mintedCount;
@@ -119,6 +36,7 @@ contract Reward is Ownable {
         _rewardState = RewardState.MintIsActive;
         _mintedCount = 0;
         _refundEscrow = new RefundEscrow(payable(msg.sender));
+        _abcRefundEscrow = new ABCRefundEscrow(payable(msg.sender));
     }
 
     /**
@@ -126,6 +44,10 @@ contract Reward is Ownable {
      */
     function totalRewards() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function totalABCRewards() public view returns (uint256) {
+        return ABCToken(ABC_TOKEN_ADDRESS).balanceOf(address(this));
     }
 
     /**
@@ -171,6 +93,7 @@ contract Reward is Ownable {
         _rewardState = RewardState.CanWithdraw;
         // 开启托管兑换
         _refundEscrow.enableRefunds();
+        _abcRefundEscrow.enableRefunds();
     }
 
     /**
@@ -179,6 +102,7 @@ contract Reward is Ownable {
     function finishWithdraw() public onlyOwner {
         _rewardState = RewardState.Finished;
         _refundEscrow.close();
+        _abcRefundEscrow.close();
     }
 
     /**
@@ -191,6 +115,12 @@ contract Reward is Ownable {
         return rewards.div(mintedNum);
     }
 
+    function _getAverageABCRewardAmount(uint256 mintedNum) internal view onlyOwner canAllocate returns (uint256) {
+        uint256 rewards = totalABCRewards();
+        require(rewards > 0, "rewards should larger than 0");
+        return rewards.div(mintedNum);
+    }
+
     /**
      * 管理员调用，给用户分配奖金
      */
@@ -199,17 +129,22 @@ contract Reward is Ownable {
         require(curMintedNum > 0, "minted number should greater than 0");
 
         uint256 rewardAmount = _getAverageRewardAmount(curMintedNum);
+        uint256 abcRewardAmount = _getAverageABCRewardAmount(curMintedNum);
+
         for (uint256 i = 0; i < curMintedNum; i++) {
             address user = _ownerOfToken(i);
             _refundEscrow.deposit{value: rewardAmount}(user);
+            _abcRefundEscrow.deposit(user, abcRewardAmount);
         }
     }
 
     /**
-     * 用户调研，查询可以取现的金额
+     * 用户调用，查询可以取现的金额
      */
-    function queryMyReward() public view canWithdraw returns (uint256) {
-        return _refundEscrow.depositsOf(msg.sender);
+    function queryMyReward() public view canWithdraw returns (uint256, uint256) {
+        uint256 ethAmount = _refundEscrow.depositsOf(msg.sender);
+        uint256 abcAmount = _abcRefundEscrow.depositsOf(msg.sender);
+        return (ethAmount, abcAmount);
     }
 
     /**
@@ -217,6 +152,7 @@ contract Reward is Ownable {
      */
     function withdrawReward() public canWithdraw {
         _refundEscrow.withdraw(payable(msg.sender));
+        _abcRefundEscrow.withdraw(msg.sender);
     }
 
     /**
@@ -224,6 +160,7 @@ contract Reward is Ownable {
      */
     function withdrawRest() public onlyOwner {
         _refundEscrow.beneficiaryWithdraw();
+        _abcRefundEscrow.beneficiaryWithdraw();
     }
 
     event ETHReceive(address, uint256);
